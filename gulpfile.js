1acedcb4592a6077f4d305c0340a60ed40c0fb4f
Bennyka1 <benjaminkampka@web.de>
2018-10-19 19:38:57 +0200

    index.php neuer Inhalt, zum test. gulpfile.js aktualisiert, css code ist nun nicht mehr "Compressed" sonder im normalen CSS layout.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 
 
 
 gulp.task('default', function() {
     gulp.src('./assets/scss/main.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(concat('main.css'))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 });
 
