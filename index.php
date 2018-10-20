4c02f91c76c595e52a48913a026567d4beda0f35
Bennyka1 <benjaminkampka@web.de>
2018-10-20 17:50:53 +0200

    Login überprüfung einfügt. Datenbank muss noch angebunden werden. Funktionalität muss geprüft werden und ggf. anpassungen gemachet werden.

--- index.php
+++ index.php
@@ -67,18 +67,6 @@
 
 </footer>
 
-
-<script> // Popup Registierung
-    // Get the modal
-    var modal = document.getElementById('id01');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-</script>
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <!--
 <script>
