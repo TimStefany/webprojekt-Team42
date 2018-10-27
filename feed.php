9573d3c399e8e017289ed2228bfd893ceeb5c554
Bennyka1 <benjaminkampka@web.de>
2018-10-27 07:10:55 +0200

    eingabe feld eingepflegt.

--- feed.php
+++ feed.php
@@ -8,7 +8,12 @@
 </nav>
 <main class="container"><!--ein Responsive Container in dem der Content steckt-->
 	<h1>Hier entsteht der geile Microblog von Team-42!</h1>
-
+    <form action="#" method="post" class="test">
+        <p><label style="color: white;">Blogeintrag:<br>
+                <textarea name="post" cols="50" rows="8" placeholder="neuer Eintrag!"></textarea></label></p>
+        <p><input type="submit" value="Posten"></p>
+    </form>
+    <!--hier kommt noch der feed-->
 </main>
 <footer>
 
