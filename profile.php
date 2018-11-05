c61225c29da0424de74dcabd04c778d67d62e1d1
Bennyka1 <benjaminkampka@web.de>
2018-11-05 15:14:43 +0100

    Navbar angepasst, Responsive bis bootstrap LG

--- profile.php
+++ profile.php
@@ -48,40 +48,30 @@
     </ul>
     </div>
 </nav>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<hr>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">Plus - Microblog</a>
+    <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Find</button>
+    </form>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find</button>
-    </form>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
                 <a class="nav-link" href="#">Feed</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Profil</a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Entdecken</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">
-                    Einstellungen
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Farbe ändern</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Edit Profile</a>
-                </div>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Profil</a>
             </li>
         </ul>
+    </div>
         <a href="#">
             <button type="button" class="btn btn-light">
                 News <span class="badge badge-light">8</span>
@@ -90,16 +80,16 @@
         <div class="d-flex"><img
                     src=https://img.fotocommunity.com/bb-bilder-9e10eb1c-ede3-47da-a2c5-97692e7faf8c.jpg?width=45&height=45
                     class="img-circle profil-image-small">
-            <a class="nav-link active dropdown-toggle" data-toggle="dropdown" href="#"> USERNAME LINK</a>
-            <ul class="dropdown-menu navbar-dark bg-dark dropdown-user-menu">
-                <li class="nav-link nav-item"><a href="#"> Abmelden</a></li>
-                <li class="nav-link"><a class="navbar-brand text-center" href="#">
-                        <i class="fas fa-cog"></i>
-                    </a></li>
-            </ul>
-
+            <a class="nav-link dropdown-toggle username" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false">
+                USERNAME
+            </a>
+            <div class="dropdown-menu dropdown-user-menu bg-primary" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Farbe ändern</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Edit Profile</a>
+            </div>
         </div>
-    </div>
 </nav>
 
 <main class="container"><!--ein Responsive Container in dem der Content steckt-->
