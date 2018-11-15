13db3fa5eebdf3e1c1d80904a952c9cdae191b79
Bennyka1 <benjaminkampka@web.de>
2018-11-15 22:19:06 +0100

    Topic wird als ID in die Datenbank zum Richtigen Post geschireben.

--- post.php
+++ post.php
@@ -1,31 +1,42 @@
 <?php
 	session_start();
-	if ( isset ( $_SESSION["signed-in"] )) {
-		$post = htmlspecialchars( $_POST["post"], ENT_QUOTES, "UTF-8" );
-		$topic = htmlspecialchars( $_POST["topic"], ENT_QUOTES, "UTF-8" );
-		$user_id =  $_SESSION["user-id"];
+	if ( isset ( $_SESSION["signed-in"] ) ) {
+		$post    = htmlspecialchars( $_POST["post"], ENT_QUOTES, "UTF-8" );
+		$topic   = htmlspecialchars( $_POST["topic"], ENT_QUOTES, "UTF-8" );
+		$user_id = $_SESSION["user-id"];
+var_dump($topic);
 
+		include_once 'userdata.php';
+		$db1    = new PDO( $dsn, $dbuser, $dbpass, $option );
+		$query1 = $db1->prepare(
+			"SELECT `topic_id` FROM `topics` WHERE `topic_name` = '".$topic."'" );
+		$query1->execute();
+		$row1 = $query1->fetch();
+		$row = $row1[0];
+var_dump($row);
 
-		if ( !empty($post) & !empty($topic)) {
-			include_once 'userdata.php';
-			try {
-				$db    = new PDO( $dsn, $dbuser, $dbpass, $option );
-				$query = $db->prepare(
-					"INSERT INTO `posts` (`user_id`,`topic_id`,`content`,`picture_id`) VALUES (:user, :topic, :post, :picture );" );
-				$query->execute( array( ":user" => $user_id, ":topic" => $topic, ":post" => $post, ":picture" => $picture, ) );
-				$db = null;
-			} catch ( PDOException $e ) {
-				echo "Error!: Bitten wenden Sie sich an den Administrator...";
-				die();
-			}
-			header( 'Location: profile.php' );
-		} else {
-			echo "Error: Bitte alle Felder ausfüllen!<br/>";
+		try {
+			$db2    = new PDO( $dsn, $dbuser, $dbpass, $option );
+			$query2= $db2->prepare(
+				"INSERT INTO `posts` (`user_id`,`topic_id`,`content`,`picture_id`) VALUES (:user, :topic, :post, :picture );" );
+			$query2->execute( array(
+				":user"    => $user_id,
+				":topic"   => $row,
+				":post"    => $post,
+				":picture" => $picture,
+			) );
+			$db2 = null;
+		} catch ( PDOException $e ) {
+			echo "Error!: Bitten wenden Sie sich an den Administrator...";
+			die();
 		}
+		header( 'Location: profile.php' );
+
+
 	} else {
 		echo '<h1>Sie sind nicht angemeldet</h1>';
 		echo '<p>gehen sie hier zu unserer Startseite und melden sie sich an</p><br>';
 		echo '<a href="index.php">Startseite</a>';
 	}
 ?>
-<?php
+
--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
     print_r($_POST);
     echo "</pre>";
 
-    if(!move_uploaded_file($_FILES['files']['tmp_name'][0], 'uploads/'.$_FILES['files']['name'][0])){
+    if(!move_uploaded_file($_FILES['files']['tmp_name'][0], '/uploads/'.$_FILES['files']['name'][0])){
         echo "error";
     }
     echo "success";
