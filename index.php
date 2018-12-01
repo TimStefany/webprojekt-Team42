43e12058e6b1c11cb95396d45ffa80b03e4f8a91
Salvatan <fs119@hdm-stuttgart.de>
2018-12-01 12:54:13 +0100

    Design Entwurf von Der Login Page,  Wichitg ist hier zu sagen das dass Modul einen seperaten hintergrund bekommt der mit der klasse .bg im Css hinterlegt ist

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>Microblog Team-42</title>
     <meta name="description" content="">
     <?php
-        include 'outsourced-php-code/header.php';
+    include 'outsourced-php-code/header.php';
     ?>
 </head>
 <body class="loginpage">
@@ -19,59 +19,62 @@
 
 </nav>
 <main class="container"><!--ein Responsive Container in dem der Content steckt-->
-    <h1>Hier entsteht der geile Microblog von Team-42!</h1>
-    <div class="text-center loginblock"> <!--Klasse wird schon von bootstrap mitgegeben-->
-        <h3 class='test'>Sie müssen sich zuerst einloggen!</h3>
-        <!-- Login-->
-        <div class="">
-            <form action="invisible-pages/login-check.php" method="post">
-                <span>Benutzername:</span><br> <input name="username" type="text"
-                                                      placeholder="Benutzername eingeben!"><br>
-                Passwort:<br> <input name="password" type="password" placeholder="Passwort eingeben!"><br>
-                <p><input name="absenden" class="btn btn-primary" type="submit" value="Login">
-                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                        Registrieren
-                    </button>
-                </p>
-            </form>
-        </div>
-    </div>
-
-
-    <!-- Modal für die Registrierung -->
-    <div class="modal fade registerblock" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Registrieren</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12"></div>
+                <div class="col-md-4 col-sm-4 col-xs-12 modal-header">
+                    <form action="invisible-pages/login-check.php" method="post" class="form-container">
+                        <h1>Plus HdM Microblog</h1>
+                        <b class="fontnorm">Benutzername:</b><br> <input class="form-control" name="username" type="text"
+                                                              placeholder="Benutzername eingeben!"><br>
+                        <b class="fontnorm">Passwort</b> <br> <input class="form-control" name="password" type="password" placeholder="Passwort eingeben!"><br>
+                        <p><input class= "form-control btn btn-dark btn-block" name="absenden"  type="submit" value="Login">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-success btn-block" data-toggle="modal"
+                                    data-target="#exampleModal">
+                                Registrieren
+                            </button>
+                        </p>
+                    </form>
                 </div>
-                <form action='invisible-pages/register.php' method='post'>
-                    <div class="modal-body">
-                        <label for="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Benutzername Eingeben" name="username" required>
-                        <br>
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Passwort Eingeben" name="password" required>
-                        <br>
-                        <label for="psw"><b>Repeat Password</b></label>
-                        <input type="password" placeholder="Passwort Wiederholen" name="password-repeat" required>
-                        <!-- Button nur vorübergehend bis die folgenden zwei funkitonstüchtig sind -->
-
-                    </div>
-                    <!-- Aktuell schließen beide Buttons das Modal und keiner führt die Registrierung aus -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zurück</button>
-                        <input type="submit" name="absenden" class="btn btn-primary" value="Registrieren" >
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
+
+
+        <!-- Modal für die Registrierung -->
+        <div class="modal fade registerblock bg" id="exampleModal" tabindex="-1" role="dialog"
+             aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title" id="exampleModalLabel">Registrieren</h1>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action='invisible-pages/register.php' method='post'>
+                        <div class="modal-body">
+                            <label for="uname"><b>Username</b></label>
+                            <input type="text" class="form-control" placeholder="Benutzername Eingeben" name="username" required>
+                            <br>
+                            <label for="psw"><b>Password</b></label>
+                            <input type="password" class="form-control" placeholder="Passwort Eingeben" name="password" required>
+                            <br>
+                            <label for="psw"><b>Repeat Password</b></label>
+                            <input type="password" class="form-control" placeholder="Passwort Wiederholen" name="password-repeat" required>
+                            <!-- Button nur vorübergehend bis die folgenden zwei funkitonstüchtig sind -->
+
+                        </div>
+                        <!-- Aktuell schließen beide Buttons das Modal und keiner führt die Registrierung aus -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Zurück</button>
+                            <input type="submit" name="absenden" class="btn btn-success" value="Registrieren">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
 </main>
 <footer>
